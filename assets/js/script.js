c99ce408f8cfe187d70f942fea0aeb28449c1678
Mark Dale <msdaledad@gmail.com>
2021-11-23 18:25:33 -0500

    still trying to debug

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,31 +32,71 @@ var reuseCityWeatherData = function (city) {
   }
 };
 
+var copyCurrentDay = function () {
+  var cd = { "city": "", "date": "", "icon": "", "temp": "", "wind": "", "humidity": "", "UVidx": "" };
+  cd.city = currentDay.city;
+  cd.date = currentDay.date;
+  cd.icon = currentDay.icon;
+  cd.temp = currentDay.temp;
+  cd.wind = currentDay.wind;
+  cd.humidity = currentDay.humidity;
+  cd.UVidx = currentDay.UVidx;
+  return cd;
+};
+
+var copyNext5Days = function () {
+  var n5d = [
+    { "date": "", "icon": "", "temp": "", "wind": "", "humidity": "", "UVidx": "" },
+    { "date": "", "icon": "", "temp": "", "wind": "", "humidity": "", "UVidx": "" },
+    { "date": "", "icon": "", "temp": "", "wind": "", "humidity": "", "UVidx": "" },
+    { "date": "", "icon": "", "temp": "", "wind": "", "humidity": "", "UVidx": "" },
+    { "date": "", "icon": "", "temp": "", "wind": "", "humidity": "", "UVidx": "" }
+  ];
+
+  for (var i = 0; i < n5d.length; i++) {
+    n5d[i] = next5Days[i].date;
+    n5d[i] = next5Days[i].icon;
+    n5d[i] = next5Days[i].temp;
+    n5d[i] = next5Days[i].wind;
+    n5d[i] = next5Days[i].humidity;
+  }
+  return n5d;
+};
+
 var setHistory = function () {
   //if not already there...add it in
   if (!foundCityWeatherData(cityName)) {
-    cityWeatherData.push({ "city": cityName, "currentDay": currentDay, "next5Days": next5Days });
+    cd = copyCurrentDay();
+    n5d = copyNext5Days();
+    cityWeatherData.push({ "city": cityName, "currentDay": cd, "next5Days": n5d });
+    console.log(cityWeatherData);
     setHistoryBtn(cityName);
   }
 };
 
 var setHistoryBtn = function (city) {
-  var historyBtnsEl = document.querySelector("#history-buttons");
   var listItemEl = document.createElement("li");
   var historyBtnEl = document.createElement("button");
   historyBtnEl.textContent = city;
   historyBtnEl.className = "btn";
   listItemEl.appendChild(historyBtnEl);
-  historyBtnsEl.appendChild(listItemEl);
-}
+  historyBtnListEl.appendChild(listItemEl);
+};
 
 var submitCity = function (event) {
   // prevent page from refreshing
   event.preventDefault();
 
+  // account for history button usage
+  if (event.target.textContent.toLowerCase() !== "search") {
+    cityName = event.target.textContent;
+    inputCityEl.value = cityName;
+  } else {
+    cityName = inputCityEl.value.trim();
+  }
+
   // get value from input element
-  console.log(inputCityEl.value.trim());
-  cityName = inputCityEl.value.trim();
+  //console.log(cityName);
 
   if (cityName) {
     if (getCityWeather(cityName)) {
@@ -73,7 +113,7 @@ const getAPIData = async (currentWeatherAPI) => {
     alert('Error: ' + response.statusText);
   }
   const json = await response.json();
-  console.log(json);
+  //console.log(json);
   return await json;
 };
 
@@ -86,7 +126,7 @@ var assignCurrentDay = function (weatherData) {
   currentDay.wind = weatherData.current.wind_speed;
   currentDay.humidity = weatherData.current.humidity;
   currentDay.UVidx = weatherData.current.uvi;
-  console.log(currentDay);
+  //console.log(currentDay);
 };
 
 var assignNext5Days = function (weatherData) {
@@ -98,10 +138,10 @@ var assignNext5Days = function (weatherData) {
     next5Days[i].wind = weatherData.daily[i].wind_speed;
     next5Days[i].humidity = weatherData.daily[i].humidity;
   }
-  console.log(next5Days);
+  //console.log(next5Days);
 };
 
-var getCityWeather = async function (cityName) {
+const getCityWeather = async function (cityName) {
 
   if (foundCityWeatherData(cityName)) {
     reuseCityWeatherData(cityName)
@@ -117,12 +157,11 @@ var getCityWeather = async function (cityName) {
   var lat = latLonJson.coord.lat;
   var lon = latLonJson.coord.lon;
 
-  // Get all weather data
   var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&appid=58ff076f4d76d50b2538a2f8d97c8d59"
   var weatherDataJson = await getAPIData(apiUrl);
 
   // Fetch all pertinent weather data for the chosen city
-  console.log(JSON.stringify(weatherDataJson));
+  //console.log(JSON.stringify(weatherDataJson));
   assignCurrentDay(weatherDataJson);
   assignNext5Days(weatherDataJson);
   fillInWeatherNow();
@@ -133,8 +172,8 @@ var getCityWeather = async function (cityName) {
 var fillInWeatherNow = function () {
   var weatherNowEl = document.querySelector("#currentDay");
   var weatherAttrs = weatherNowEl.getElementsByTagName("li");
-  console.log("filling in weather");
-  console.log(currentDay);
+  //console.log("filling in weather");
+  //console.log(currentDay);
   for (var i = 0; i < weatherAttrs.length; i++) {
     switch (i) {
       case 0:
@@ -162,23 +201,23 @@ var fillInWeatherNow = function () {
         break;
     }
   }
-}
+};
 
 var removeAllChildNodes = function (parent) {
-  console.log("START OF removeAllChildNodes()");
+  //console.log("START OF removeAllChildNodes()");
   if (parent) {
     while (parent.firstChild) {
       parent.removeChild(parent.firstChild);
     }
   }
-  console.log("END OF removeAllChildNodes()");
-}
+  //console.log("END OF removeAllChildNodes()");
+};
 
 
 var fillInNext5Days = function () {
   var weatherNext5DaysEl = document.querySelector("#next5Days");
   var days = weatherNext5DaysEl.getElementsByTagName("li");
-  console.log("filling in forecast");
+  //console.log("filling in forecast");
   for (var i = 0; i < days.length; i++) {
     if (days[i].getElementsByTagName("p")) {
       removeAllChildNodes(days[i]);
@@ -210,9 +249,12 @@ var fillInNext5Days = function () {
       }
       days[i].appendChild(weatherAttrEl);
     }
-    console.log(days[i]);
+    //console.log(days[i]);
   }
 };
 
 inputCityBtnEl.addEventListener('click', submitCity);
+//document.querySelectorAll("#history-buttons").forEach(item => {
+//  item.addEventListener("click", submitCity);
+//});
 historyBtnListEl.addEventListener('click', submitCity);
